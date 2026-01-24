64c7e0eb0d905c1acf7979416c5ff8653dd55019
Lucas Hartzell <hartzelllucas1@gmail.com>
2026-01-24 02:41:01 -0500

    Added a way to add new tasks and account for prioirty change

--- backend/main.py
+++ backend/main.py
@@ -4,3 +4,14 @@ currentTasks = task.getTasks()
 
 # Add new task
 def addTask(description, priority):
+	newTask = Task(description, priority)
+	fixPriority(priority)
+
+	currentTasks.append(newTask)
+	currentTasks = task.sortTasks(currentTasks)
+
+# Account for priority change
+def fixPriority(newPriority):
+	for item in currentTasks:
+		if item.priority <= newPriority:
+			item.priority += 1

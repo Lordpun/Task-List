5718af09e5541b494b424475035a696fcb11261c
Lucas Hartzell <hartzelllucas1@gmail.com>
2026-01-28 16:25:20 -0500

    Filled out API functions

--- backend/api.py
+++ backend/api.py
@@ -1,24 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import main
 
 app = Flask(__name__)
 
 @app.route("/task/list", methods=["GET"])
 def getTasks():
-	return jsonify(main.get_tasks())
+	return jsonify(main.get_tasks()), 200
 
-@app.route("/task/add/<description>", methods=["POST"])
-def addTask(description):
-	pass
+@app.route("/task/add", methods=["POST"])
+def addTask():
+	data = request.get_json()
+	description = data.get("description")
 
-@app.route("/task/remove/<priority>", methods=["DELETE"])
-def removeTask(priority):
-	pass
+	main.addTask(description)
 
-@app.route("/task/changePriority/up/<priority>", methods=["PUT"])
-def changePriorityUp(priority):
-	pass
+	return "Added task succesfully", 201
 
-@app.route("/task/changePriority/down/<priority>", methods=["PUT"])
-def changePriorityDown(priority):
-	pass
+@app.route("/task/remove", methods=["DELETE"])
+def removeTask():
+	data = request.get_json()
+	priority = data.get("priority")	
+
+	main.removeTask(priority)
+
+	return "Removed task succesfully", 204
+
+@app.route("/task/changePriority", methods=["PUT"])
+def changePriority():
+	data = request.get_json()
+	priority = data.get("priority")
+	direction = data.get("direction")
+
+	main.changePriority(priority, direction)
+
+	return "Changed priority sucesfully", 200

22d9db4d59ccf42e3110d90efa47954630a7e1cc
Lucas Hartzell <hartzelllucas1@gmail.com>
2026-01-28 18:40:05 -0500

    Replaced API in favor of running Python directly through Tauri

--- frontend/src-tauri/build.rs
+++ frontend/src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
   tauri_build::build()
 }
+
+tauri::Builder::default()
+  .plugin(tauri_plugin_python::init())
+  .run(tauri::generate_context!())
+  .expect("error while running tauri application");

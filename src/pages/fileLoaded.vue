e667d8b67c49478e07d06c2bc6150d80f489d6de
Lucas Hartzell <hartzelllucas1@gmail.com>
2026-01-30 16:19:18 -0500

    Setup Vue Router and started working on pages

---
+++ src/pages/fileLoaded.vue
@@ -0,0 +1,52 @@
+<template>
+  <section>
+      <h4>Your Current Tasks</h4>
+    </section>
+
+    <ul v-if="tasksExist" class="taskBody">
+      <task
+      v-for="(task, index) in tasksLoaded"
+      :key="index"
+      v-bind="task"
+       />
+    </ul>
+
+    <ul v-else>
+      <h4>No tasks exist</h4>
+    </ul>
+
+    <section class="taskInput">
+      <inputField></inputField>
+    </section>
+</template>
+
+<script setup>
+  import task from "./components/task.vue"
+  import inputField from "./components/input.vue"
+  import { invoke } from '@tauri-apps/api/core';
+  import { ref, onMounted } from 'vue';
+
+  const tasksLoaded = ref([])
+  const tasksExist = ref(true)
+
+  async function displayTasks() {
+    const taskJson = await callFunction("get_tasks");
+    tasksLoaded.value = JSON.parse(taskJson);
+
+    if ( tasksLoaded.value.length <= 0 ) { tasksExist.value = false; }
+    else { tasksExist.value = true; }
+  }
+
+  onMounted(() => {
+    displayTasks();
+  });
+</script>
+
+<style scoped>
+  section {
+    border: 1px solid #ccc;
+    width: 90%;
+    margin: 0 auto;
+    text-align: center;
+  }
+</style>
---
+++ src/pages/noFile.vue
@@ -0,0 +1,20 @@
+<template>
+	<section>
+		<h4>No file selected</h4>
+		<p>Select one here:</p>
+		<input type="file" @change="getFile" acecept=".json">
+	</section>
+</template>
+
+<script setup>
+	function getFile() {
+		const file = this.files[0];
+
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			console.log("Not implemented yet");
+		} catch (error) {
+			console.log("Invalid JSON file");
+		}
+	}
+</script>
